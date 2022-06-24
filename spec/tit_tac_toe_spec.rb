062674feaed207a1df16f2d132747e5833c07730
Andy <andy.ferrie@gmail.com>
2022-06-24 15:56:40 +0100

    print empty board
    Co-authored-by: Sabrina <contact@saromsa.com>

--- spec/tit_tac_toe_spec.rb
+++ spec/tit_tac_toe_spec.rb
@@ -1,12 +1,34 @@
 require_relative("../tic_tac_toe.rb")
 
 describe "Tic Tac Toe" do
+
     tictactoe = TicTacToe.new
-    it "has 3 values in the array" do
-        expect(tictactoe.placement_array.length).to eq(3)
+    it "has 9 values in the array" do
+        expect(tictactoe.placement_array.length).to eq(9)
     end
 
-    it "has a total of 9 values in 2d array" do
-        expect(tictactoe.placement_array.flatten.length).to eq(9)
+    it "creates player one and assigns X" do
+        expect(tictactoe.player_one).to eq("X")
+    end 
+
+    it "creates player two and assigns O" do
+        expect(tictactoe.player_two).to eq("O")
+    end 
+
+    it "print board outputs something" do
+        expect { tictactoe.print_board }. to output.to_stdout   
     end
+
+    it "prints the lines of the board" do
+        expect { tictactoe.print_board }.to output(/---|---|---/).to_stdout
+    end
+
+    it "prints an empty board" do
+        expect { tictactoe.print_board }.to output(/ 1 | 2 | 3 /).to_stdout
+        expect { tictactoe.print_board }.to output(/ 4 | 5 | 6 /).to_stdout
+        expect { tictactoe.print_board }.to output(/ 7 | 8 | 9 /).to_stdout
+    end
+        
 end
+
+      
--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,21 +1,27 @@
 class TicTacToe
     attr_accessor :placement_array
     def initialize
-        @placement_array = [['_', '_', '_'],['_', '_', '_'],['_', '_', '_']]
+        @placement_array = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+    end
+
+    def player_one
+        player_one = "X"
+    end
+
+    def player_two
+        player_two = "O"
+    end
+
+    def print_board
+        puts " 1 | 2 | 3 "
+        puts "---|---|---"
+        puts " 4 | 5 | 6 "
+        puts "---|---|---"
+        puts " 7 | 8 | 9 "
     end
 end
 
 
-# array = [
-#     ['_', '_', '_']
-#     []
-#     []
-# ]
+
     
 
-#     A1 = [1][1]
-
-#     print  1 2 3
-#     print "A #{array[0][0]} | #{array[0][1] | }"
-#     print B [ ]
-#     print C [ ]
