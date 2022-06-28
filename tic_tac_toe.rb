8c1d6671440b7130de5c53052d9936b5c78ae7aa
Andy <andy.ferrie@gmail.com>
2022-06-28 12:36:01 +0100

    check win logic

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,15 +1,10 @@
 class TicTacToe
-    attr_accessor :board
+    attr_accessor :board, :player_one, :player_two, :winning_combinations
     def initialize
         @board = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-    end
-
-    def assign_player_one
-        player_one = "X"
-    end
-
-    def assign_player_two
-        player_two = "O"
+        @player_one = "X"
+        @player_two = "O"
+        @winning_combinations = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [3, 5, 7], [1, 5, 9]]
     end
 
     def print_board(board = @board)
@@ -21,14 +16,33 @@ class TicTacToe
     end
 
     def move(player, cell)
-        if board[cell - 1] == "X" or board[cell - 1] == "O"
-            print "Cell already taken"
+        if board[cell - 1] == 'X' or board[cell - 1] == 'O'
+            puts "Cell already taken"
         else
             board[cell - 1] = player
         end
     end 
 
+    def check_for_win
+        if board[0] == 'X' and board[1] == 'X' and board[2] == 'X'
+        puts "Player 1 wins!"
+        end
+    end
+
+        
+    def winning_combinations.map do |combination| 
+        count = 0
+        combination.map do |cell|
+            if player_one_moves.include?(cell)
+                count++
+            if count == 3
+                puts "player one wins!"
+            end
+        end
+    end
+        
 end
+
 
 
 
