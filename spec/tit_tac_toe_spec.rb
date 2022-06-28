c2109515bf1f2b1b35695318a7def03d3fd53b88
Andy <andy.ferrie@gmail.com>
2022-06-28 11:27:59 +0100

    updates values and checks for duplicate
    Co-authored-by: Sabrina <contact@saromsa.com>

--- spec/tit_tac_toe_spec.rb
+++ spec/tit_tac_toe_spec.rb
@@ -4,15 +4,15 @@ describe "Tic Tac Toe" do
 
     tictactoe = TicTacToe.new
     it "has 9 values in the array" do
-        expect(tictactoe.placement_array.length).to eq(9)
+        expect(tictactoe.board.length).to eq(9)
     end
 
     it "creates player one and assigns X" do
-        expect(tictactoe.player_one).to eq("X")
+        expect(tictactoe.assign_player_one).to eq("X")
     end 
 
     it "creates player two and assigns O" do
-        expect(tictactoe.player_two).to eq("O")
+        expect(tictactoe.assign_player_two).to eq("O")
     end 
 
     it "print board outputs something" do
@@ -23,19 +23,49 @@ describe "Tic Tac Toe" do
         expect { tictactoe.print_board }.to output(/---\|---\|---/).to_stdout
     end
 
-    it "prints an empty board" do
+    it "prints an empty board with numbered cells" do
         expect { tictactoe.print_board }.to output(/ 1 \| 2 \| 3 \n/).to_stdout
         expect { tictactoe.print_board }.to output(/ 4 \| 5 \| 6 \n/).to_stdout
         expect { tictactoe.print_board }.to output(/ 7 \| 8 \| 9 \n/).to_stdout
     end
 
-    context "prints an empty board" do
-        placement_array = ['X', 'X', 'X', '4', '5', '6', '7', '8', '9']
-        specify { expect { print (tictactoe.print_board(placement_array)) }.to output(/ X \| X \| X /).to_stdout }
-        # expect { tictactoe.print_board(placement_array) }.to output(/ 4 | 5 | 6 /).to_stdout
-        # expect { tictactoe.print_board(placement_array) }.to output(/ 7 | 8 | 9 /).to_stdout
+    context "prints a empty board with updated cell values" do
+        board = ['X', 'X', 'X', '4', '5', '6', '7', '8', '9']
+        specify { expect { tictactoe.print_board(board) }.to output(/ X \| X \| X /).to_stdout }
+        specify { expect { tictactoe.print_board(board) }.to output(/ 4 \| 5 \| 6 /).to_stdout }
+        specify { expect { tictactoe.print_board(board) }.to output(/ 7 \| 8 \| 9 /).to_stdout }
     end
-        
+
+    it "updates board when player one makes a move" do
+        player_one = tictactoe.assign_player_one
+        tictactoe.move(player_one, 1)
+        expect(tictactoe.board).to eq(['X', '2', '3', '4', '5', '6', '7', '8', '9'])
+    end
+
+    it "updates cell when player one make a move" do
+        tictactoe = TicTacToe.new
+        player_one = tictactoe.assign_player_one
+        tictactoe.move(player_one, 1)
+        expect(tictactoe.board[0]).to eq('X')
+    end
+
+    it "updates board when player one and player two make moves" do
+        tictactoe = TicTacToe.new
+        player_one = tictactoe.assign_player_one
+        player_two = tictactoe.assign_player_two
+        tictactoe.move(player_one, 1)
+        tictactoe.move(player_two, 3)
+        expect(tictactoe.board).to eq(['X', '2', 'O', '4', '5', '6', '7', '8', '9'])
+    end
+    
+    it "doesn't update board when player two selects the same cell as and player one" do
+        tictactoe = TicTacToe.new
+        player_one = tictactoe.assign_player_one
+        player_two = tictactoe.assign_player_two
+        tictactoe.move(player_one, 1)
+        tictactoe.move(player_two, 1)
+        expect(tictactoe.board).to eq(['X', '2', '3', '4', '5', '6', '7', '8', '9'])
+    end
 end
 
       
--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,24 +1,33 @@
 class TicTacToe
-    attr_accessor :placement_array
+    attr_accessor :board
     def initialize
-        @placement_array = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+        @board = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
     end
 
-    def player_one
+    def assign_player_one
         player_one = "X"
     end
 
-    def player_two
+    def assign_player_two
         player_two = "O"
     end
 
-    def print_board(placement_array = @placement_array)
-        puts " #{placement_array[0]} | #{placement_array[1]} | #{placement_array[2]} "
+    def print_board(board = @board)
+        puts " #{board[0]} | #{board[1]} | #{board[2]} "
         puts "---|---|---"
-        puts " #{placement_array[3]} | #{placement_array[4]} | #{placement_array[5]} "
+        puts " #{board[3]} | #{board[4]} | #{board[5]} "
         puts "---|---|---"
-        puts " #{placement_array[6]} | #{placement_array[7]} | #{placement_array[8]} "
+        puts " #{board[6]} | #{board[7]} | #{board[8]} "
     end
+
+    def move(player, cell)
+        if board[cell - 1] == "X" or board[cell - 1] == "O"
+            print "Cell already taken"
+        else
+            board[cell - 1] = player
+        end
+    end 
+
 end
 
 
